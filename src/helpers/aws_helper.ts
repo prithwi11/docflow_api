43e09d8ff082715d0322d0ace4c87086df010026
prithwiraj-msspl <prithwiraj.bhadra@massoftind.com>
2026-03-07 13:30:35 +0530

    setting up localstack

--- src/helpers/aws_helper.ts
+++ src/helpers/aws_helper.ts
@@ -19,7 +19,6 @@ export class aws_helper {
                     secretAccessKey: this._config.awsSecretAccessKey as string
                 }
             });
-            console.log("s3Client", this.client)
         }
         else {
             this.client = new S3Client({
